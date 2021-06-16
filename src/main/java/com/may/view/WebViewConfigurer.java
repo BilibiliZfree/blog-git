b0c7908e7e8860afbdf56093b9f961d6904a747e
紫枫伊 <985463602@qq.com>
2021-06-16 17:05:19 +0800

    添加了视图控制器WebViewConfigurer

---
+++ src/main/java/com/may/view/WebViewConfigurer.java
@@ -0,0 +1,12 @@
+package com.may.view;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author 紫枫伊
+ * @creatDate 2021/6/16 17:03
+ * @updateDate 2021/6/16 17:03
+ * @description 视图控制器
+ */
+public class WebViewConfigurer implements WebMvcConfigurer {
+}
