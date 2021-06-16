c0cbd088d99d85f85b5edc4264f2583f5f6246de
紫枫伊 <985463602@qq.com>
2021-06-16 17:38:08 +0800

    登录页引入thymeleaf模板

--- src/main/java/com/may/view/WebViewConfigurer.java
+++ src/main/java/com/may/view/WebViewConfigurer.java
@@ -1,12 +1,22 @@
 package com.may.view;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author 紫枫伊
  * @creatDate 2021/6/16 17:03
- * @updateDate 2021/6/16 17:03
+ * @updateDate 2021/6/16 17:14
  * @description 视图控制器
  */
+@Configuration
 public class WebViewConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        WebMvcConfigurer.super.addViewControllers(registry);
+
+        /*登录页*/
+        registry.addViewController("/index.html").setViewName("admin/index");
+    }
 }
