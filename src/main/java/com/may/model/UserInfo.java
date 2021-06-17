b2c7280e4fdb3ff207c3128ec9ffdc3fcf94f0c0
紫枫伊 <985463602@qq.com>
2021-06-17 18:39:12 +0800

    设计了用户信息表相对应的实体类

---
+++ src/main/java/com/may/model/UserInfo.java
@@ -0,0 +1,161 @@
+package com.may.model;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+import java.util.Date;
+
+/**
+ * @author 紫枫伊
+ * @creatDate 2021/6/17 17:56
+ * @updateDate 2021/6/17 17:56
+ * @description 用户信息表
+ */
+@Entity
+@Table(name = "userinfo")
+public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private BigInteger id;
+    private String account;
+    private String password;
+    private String nickname;
+    private String email;
+    private String telephone;
+    private String avatar;
+    private Integer gender;
+    private BigInteger account_balance;
+    private Integer status;
+    private Integer permission;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date create_time;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date update_time;
+
+    public UserInfo() {
+    }
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public BigInteger getAccount_balance() {
+        return account_balance;
+    }
+
+    public void setAccount_balance(BigInteger account_balance) {
+        this.account_balance = account_balance;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getPermission() {
+        return permission;
+    }
+
+    public void setPermission(Integer permission) {
+        this.permission = permission;
+    }
+
+    public Date getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Date create_time) {
+        this.create_time = create_time;
+    }
+
+    public Date getUpdate_time() {
+        return update_time;
+    }
+
+    public void setUpdate_time(Date update_time) {
+        this.update_time = update_time;
+    }
+
+    @Override
+    public String toString() {
+        return "UserInfo{" +
+                "id=" + id +
+                ", account='" + account + '\'' +
+                ", password='" + password + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", email='" + email + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", avatar='" + avatar + '\'' +
+                ", gender=" + gender +
+                ", account_balance=" + account_balance +
+                ", status=" + status +
+                ", permission=" + permission +
+                ", create_time=" + create_time +
+                ", update_time=" + update_time +
+                '}';
+    }
+}
