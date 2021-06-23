d7dd1d0de3abfd8abd4e161133f6a163de8fc94a
紫枫伊 <985463602@qq.com>
2021-06-23 14:58:50 +0800

    添加了404页面和500页面并新增http状态判断

--- src/main/java/com/may/view/WebViewConfigurer.java
+++ src/main/java/com/may/view/WebViewConfigurer.java
@@ -1,8 +1,10 @@
 package com.may.view;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 /**
  * @author 紫枫伊
@@ -14,8 +16,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebViewConfigurer implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        WebMvcConfigurer.super.addViewControllers(registry);
-
+        // 这里前提得有404和500这一个html存在，不然他会返回他默认的404页面
+        registry.addViewController("/404").setStatusCode(HttpStatus.NOT_FOUND).setViewName("message/404");
+        registry.addViewController("/500").setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR).setViewName("message/500");
         /*登录页*/
         registry.addViewController("/index.html").setViewName("admin/index");
         /*注册页*/
